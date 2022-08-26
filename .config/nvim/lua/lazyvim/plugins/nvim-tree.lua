d57011c4f9dd1576d12c49ec8603cb2f2af2ee9e
Hao Nguyen <hao@haonguyen.tech>
2022-08-26 12:00:00 +0700

    feat: disable nvim-tree

--- .config/nvim/lua/lazyvim/plugins/nvim-tree.lua
+++ .config/nvim/lua/lazyvim/plugins/nvim-tree.lua
@@ -1,7 +1,7 @@
 --Nvim-Tree file explorer
 return {
   "nvim-tree/nvim-tree.lua",
-  enabled = true,
+  enabled = false,
   dependencies = {
     "nvim-tree/nvim-web-devicons",
     opt = true, -- optional, for file icons
