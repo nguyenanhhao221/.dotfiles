ed0b9c0baf969430a3b47538d2492a993be2691d
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2023-03-17 23:31:00 +0700

    fix for auto tag in treesitter

--- .config/nvim/lua/lazyvim/plugins/treesitter.lua
+++ .config/nvim/lua/lazyvim/plugins/treesitter.lua
@@ -10,7 +10,21 @@ return {
     -- Additional text objects via treesitter
     dependencies = {
       "nvim-treesitter/nvim-treesitter-textobjects",
-      "windwp/nvim-ts-autotag",
+      {
+        "windwp/nvim-ts-autotag",
+        config = function()
+          vim.lsp.handlers["textDocument/publishDiagnostics"] =
+            vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+              underline = true,
+              virtual_text = {
+                spacing = 5,
+                severity_limit = "Warning",
+              },
+              update_in_insert = true,
+            })
+          require("nvim-ts-autotag").setup()
+        end,
+      },
       "windwp/nvim-autopairs",
     },
   },
