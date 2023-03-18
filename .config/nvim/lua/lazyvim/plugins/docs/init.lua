1c0672c0904eb41f69ac9e9b92e784476402fc18
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2023-03-18 12:52:39 +0700

    feat: add plugin for docs support
    Neogen
    https://github.com/danymat/neogen

---
+++ .config/nvim/lua/lazyvim/plugins/docs/init.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    cmd = "Neogen",
+    -- Uncomment next line if you want to follow only stable versions
+    -- version = "*",
+    opts = {
+      snippet_engine = "luasnip",
+    },
+  },
+}
