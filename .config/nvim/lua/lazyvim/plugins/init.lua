ff142ac755b595db3d7146aa385829861c413256
Hao Nguyen <32004536+nguyenanhhao221@users.noreply.github.com>
2023-03-21 23:37:00 +0700

    feat(util): add markdown preview plugin together with glow
    - Glow only preview markdown inside nvim, which sometime not working
      properly
    - This plugin can trigger a preview with default browser

--- .config/nvim/lua/lazyvim/plugins/init.lua
+++ .config/nvim/lua/lazyvim/plugins/init.lua
@@ -151,6 +151,13 @@ return {
   },
   --Markdown preview
   { "ellisonleao/glow.nvim", config = true, cmd = "Glow" },
+  {
+    "iamcco/markdown-preview.nvim",
+    ft = "markdown",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
   --A pretty list for showing diagnostics, references, telescope results, quickfix and location lists to help you solve all the trouble your code is causing.
   {
     "folke/trouble.nvim",
