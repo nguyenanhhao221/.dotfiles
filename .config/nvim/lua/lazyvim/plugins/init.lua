9e1f7a096315f33b547efd8130d56c3febc20cdf
Hao Nguyen <hao@haonguyen.tech>
2023-03-25 00:23:00 +0700

    refactor: move colorschemes seprate, add rose-pine

--- .config/nvim/lua/lazyvim/plugins/init.lua
+++ .config/nvim/lua/lazyvim/plugins/init.lua
@@ -29,48 +29,6 @@ return {
     end,
   },
 
-  -- Theme, there are multiple theme, comment and uncomment which one to use
-  -- Tokyonight themes
-  {
-    "folke/tokyonight.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-    name = "tokyonight",
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme tokyonight-night]])
-    end,
-    style = "night",
-  },
-  --Kanagawa Themes
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = true, -- make sure we load this during startup if it is your main colorscheme
-    -- priority = 1000, -- make sure to load this before all the other start plugins
-    name = "kanagawa",
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme kanagawa]])
-      require("kanagawa").setup({
-        compile = true, -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = true, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        theme = "wave", -- Load "wave" theme when 'background' option is not set
-        background = {
-          -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          light = "lotus",
-        },
-      })
-    end,
-  },
   { "nvim-treesitter/playground", lazy = true },
 
   --Harpoon to navigate files by ThePrimeagen
