9355f3f471e131c89b6640ddb44ebdf48175bc72
Hao Nguyen <hao@haonguyen.tech>
2023-03-27 03:36:14 +0700

    feat(lsp): update config style for later

--- .config/nvim/lua/lazyvim/plugins/lsp/init.lua
+++ .config/nvim/lua/lazyvim/plugins/lsp/init.lua
@@ -69,6 +69,18 @@ return {
         },
       },
     },
+    -- you can do any additional lsp server setup here
+    -- return true if you don't want this server to be setup with lspconfig
+    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+    setup = {
+      -- example to setup with typescript.nvim
+      -- tsserver = function(_, opts)
+      --   require("typescript").setup({ server = opts })
+      --   return true
+      -- end,
+      -- Specify * to use this function as a fallback for any server
+      -- ["*"] = function(server, opts) end,
+    },
     config = function(_, opts)
       -- LSP settings.
       --  This function gets run when an LSP connects to a particular buffer.
@@ -176,7 +188,7 @@ return {
       { "nvim-treesitter/nvim-treesitter" },
       { "onsails/lspkind.nvim" },
     },
-    event = { "BufReadPre", "BufNewFile" },
+    event = "BufRead",
     opts = {
       -- keybinds for navigation in lspsaga window
       scroll_preview = { scroll_down = "<C-f>", scroll_up = "<C-b>" },
