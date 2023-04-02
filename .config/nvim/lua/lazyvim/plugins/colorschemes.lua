b63c47b7a7d68c08cefb47e4521ad763fe85a8cc
Hao Nguyen <hao@haonguyen.tech>
2023-04-02 15:22:11 +0700

    new update

--- .config/nvim/lua/lazyvim/plugins/colorschemes.lua
+++ .config/nvim/lua/lazyvim/plugins/colorschemes.lua
@@ -30,7 +30,7 @@ return {
         statementStyle = { bold = true },
         typeStyle = {},
         transparent = true, -- do not set background color
-        dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+        dimInactive = true, -- dim inactive window `:h hl-NormalNC`
         terminalColors = true, -- define vim.g.terminal_color_{0,17}
         theme = "wave", -- Load "wave" theme when 'background' option is not set
         background = {
