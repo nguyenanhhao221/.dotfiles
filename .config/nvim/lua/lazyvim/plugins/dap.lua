c4f5afcff3a806a470f1012f1a4b1f578f1a2ce9
Hao Nguyen <hao@haonguyen.tech>
2023-04-06 02:30:53 +0700

    fix(dap): adjust build command of vscode-debug-js

--- .config/nvim/lua/lazyvim/plugins/dap.lua
+++ .config/nvim/lua/lazyvim/plugins/dap.lua
@@ -89,7 +89,7 @@ return {
   {
     "microsoft/vscode-js-debug",
     lazy = true,
-    build = "npm install --legacy-peer-deps && npm run compile",
+    build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
   },
   {
     "rcarriga/nvim-dap-ui",
