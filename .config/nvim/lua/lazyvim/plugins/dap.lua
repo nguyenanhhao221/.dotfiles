26dac47baa062142c73f4fc40aad8a389fdf2424
Hao Nguyen <hao@haonguyen.tech>
2023-04-08 17:14:07 +0700

    feat(dap): test out new config for nextjs debug

--- .config/nvim/lua/lazyvim/plugins/dap.lua
+++ .config/nvim/lua/lazyvim/plugins/dap.lua
@@ -56,6 +56,26 @@ return {
             webRoot = "${workspaceFolder}",
             userDataDir = "${workspaceFolder}/.vscode/vscode-chrome-debug-userdatadir",
           },
+          {
+            name = "Next.js: debug full stack",
+            type = "node-terminal",
+            request = "launch",
+            command = "pnpm run dev",
+            serverReadyAction = {
+              pattern = "started server on .+, url: (https?://.+)",
+              uriFormat = "%s",
+              action = "debugWithChrome",
+            },
+            cwd = "${workspaceFolder}",
+          },
+          {
+            type = "pwa-node",
+            request = "attach",
+            name = "Attach Program (pwa-node, select pid)",
+            cwd = vim.fn.getcwd(),
+            processId = require("dap.utils").pick_process,
+            skipFiles = { "<node_internals>/**" },
+          },
         }
       end
     end,
@@ -95,10 +115,57 @@ return {
     "rcarriga/nvim-dap-ui",
     event = "VeryLazy",
     dependencies = { "mfussenegger/nvim-dap" },
+    opts = {
+
+      icons = { expanded = "▾", collapsed = "▸" },
+      mappings = {
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t",
+      },
+      expand_lines = vim.fn.has("nvim-0.7"),
+      layouts = {
+        {
+          elements = {
+            -- Elements can be strings or table with id and size keys.
+            { id = "scopes", size = 0.25 },
+            "breakpoints",
+            "stacks",
+            "watches",
+          },
+          size = 40,
+          position = "left",
+        },
+        {
+          elements = {
+            { id = "repl", size = 0.5 },
+            { id = "console", size = 0.5 },
+          },
+          size = 10,
+          position = "bottom",
+        },
+      },
+      floating = {
+        max_height = nil, -- These can be integers or a float between 0 and 1.
+        max_width = nil, -- Floats will be treated as percentage of your screen.
+        border = "rounded", -- Border style. Can be "single", "double" or "rounded"
+        mappings = {
+          close = { "q", "<Esc>" },
+        },
+      },
+      windows = { indent = 1 },
+      render = {
+        max_type_length = nil,
+      },
+    },
     config = function()
       local dap, dapui = require("dap"), require("dapui")
       dap.listeners.after.event_initialized["dapui_config"] = function()
-        dapui.open({})
+        vim.cmd("tabfirst|tabnext")
+        dapui.open()
       end
       dap.listeners.before.event_terminated["dapui_config"] = function()
         dapui.close({})
