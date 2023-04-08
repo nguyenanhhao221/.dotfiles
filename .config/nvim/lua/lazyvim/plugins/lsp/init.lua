19abc9c3916a8449f0a504142bd360933edd0753
Hao Nguyen <hao@haonguyen.tech>
2023-04-08 17:13:51 +0700

    refactor(lsp): reuse lsp-saga hover

--- .config/nvim/lua/lazyvim/plugins/lsp/init.lua
+++ .config/nvim/lua/lazyvim/plugins/lsp/init.lua
@@ -103,8 +103,8 @@ return {
           vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
         end
         -- Hover docs
-        -- nmap("<leader>k", ":Lspsaga hover_doc<CR>", "Hover Docs")
-        nmap("<leader>k", vim.lsp.buf.hover, "Hover Docs")
+        nmap("<leader>k", ":Lspsaga hover_doc<CR>", "Hover Docs")
+        -- nmap("<leader>k", vim.lsp.buf.hover, "Hover Docs")
         -- Code action
         nmap("<leader>ca", ":Lspsaga code_action<CR>", "[c]ode [a]ction")
         -- Go to definition
