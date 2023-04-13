6e1987afd4712c2225622a9cb973f683439a43d5
Hao Nguyen <hao@haonguyen.tech>
2023-04-13 21:56:15 +0700

    feat(treesitter): enable regex highlight in typescript

--- .config/nvim/lua/lazyvim/plugins/treesitter.lua
+++ .config/nvim/lua/lazyvim/plugins/treesitter.lua
@@ -80,7 +80,7 @@ return {
       -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
       -- Using this option may slow down your editor, and you may see some duplicate highlights.
       -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
+      additional_vim_regex_highlighting = { "typescript" },
     },
     indent = { enable = true, disable = { "python" } },
     autotag = { enable = true },
