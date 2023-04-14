b40800ae78b4241550f7b1d7c7c4f7d12f699ae8
Hao Nguyen <hao@haonguyen.tech>
2023-04-15 01:20:35 +0700

    fix(treesitter): syntax problem, treesitter now load correctly

--- .config/nvim/lua/lazyvim/plugins/treesitter.lua
+++ .config/nvim/lua/lazyvim/plugins/treesitter.lua
@@ -25,70 +25,70 @@ return {
       },
       "windwp/nvim-autopairs",
     },
-  },
-  opts = {
-    -- A list of parser names, or "all" (the four listed parsers should always be installed)
-    ensure_installed = {
-      "c",
-      "cpp",
-      "go",
-      "lua",
-      "python",
-      "rust",
-      "typescript",
-      "help",
-      "vim",
-      "markdown_inline",
-      "markdown",
-      "regex",
-      "bash",
-      "tsx",
-      "gitignore",
-      "javascript",
-      "json",
-      "css",
-      "html",
-    },
-    -- Install parsers synchronously (only applied to `ensure_installed`)
-    sync_install = false,
+    ---@param opts TSConfig
+    opts = {
+      -- A list of parser names, or "all" (the four listed parsers should always be installed)
+      ensure_installed = {
+        "c",
+        "cpp",
+        "go",
+        "lua",
+        "python",
+        "rust",
+        "typescript",
+        "vimdoc",
+        "vim",
+        "markdown_inline",
+        "markdown",
+        "regex",
+        "bash",
+        "tsx",
+        "gitignore",
+        "javascript",
+        "json",
+        "css",
+        "html",
+      },
+      -- Install parsers synchronously (only applied to `ensure_installed`)
+      sync_install = false,
 
-    -- Automatically install missing parsers when entering buffer
-    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-    auto_install = true,
+      -- Automatically install missing parsers when entering buffer
+      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+      auto_install = true,
 
-    textobjects = {
-      select = {
-        enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ["aa"] = "@parameter.outer",
-          ["ia"] = "@parameter.inner",
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-          ["ac"] = "@class.outer",
-          ["ic"] = "@class.inner",
+      textobjects = {
+        select = {
+          enable = true,
+          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            ["aa"] = "@parameter.outer",
+            ["ia"] = "@parameter.inner",
+            ["af"] = "@function.outer",
+            ["if"] = "@function.inner",
+            ["ac"] = "@class.outer",
+            ["ic"] = "@class.inner",
+          },
         },
       },
-    },
 
-    highlight = {
-      -- `false` will disable the whole extension
-      enable = true,
+      highlight = {
+        -- `false` will disable the whole extension
+        enable = true,
 
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true, disable = { "python" } },
+      autotag = { enable = true },
     },
-    indent = { enable = true, disable = { "python" } },
-    autotag = { enable = true },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
   },
-  ---@param opts TSConfig
-  config = function(_, opts)
-    require("nvim-treesitter.configs").setup(opts)
-  end,
   --Treesitter context for sticky scroll
   {
     "nvim-treesitter/nvim-treesitter-context",
