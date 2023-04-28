b8fec78f1e21a064e7e773efab982b2a0a377d13
Hao Nguyen <hao@haonguyen.tech>
2023-04-29 05:10:02 +0700

    feat(lsp): config pylsp direct in nvim-lsp-config
    - better logic to handle mason lsp config
    - use setup and setup_handler to go through each server config and put
      them through mason instead

--- .config/nvim/lua/lazyvim/plugins/lsp/init.lua
+++ .config/nvim/lua/lazyvim/plugins/lsp/init.lua
@@ -39,35 +39,68 @@ return {
         },
         severity_sort = true,
       },
+      -- add any global capabilities here
+      capabilities = {},
       -- Automatically format on save
       autoformat = true,
       format = {
         formatting_options = nil,
         timeout_ms = nil,
       },
+      -- LSP Server Settings
       --  Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
       --  Add any additional override configuration in the following tables. They will be passed to
+      ---@type lspconfig.options
       servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
-        -- tsserver = {},
-        -- rust_analyzer = {},
-        lua_ls = {
-          Lua = {
-            workspace = {
-              checkThirdParty = false,
-            },
-            telemetry = { enable = false },
-            diagnostics = {
-              globals = "vim",
+        jsonls = {},
+        pylsp = {
+          -- For debugging
+          -- cmd = { os.getenv("HOME") .. "/.config/nvim/venv/bin/pylsp", "--log-file=/tmp/pylsp.log", "-v" },
+          settings = {
+            pylsp = {
+              configurationSources = { "flake8" },
+              plugins = {
+                flake8 = {
+                  enabled = true,
+                },
+                pycodestyle = {
+                  enabled = false,
+                },
+                mccabe = {
+                  enabled = false,
+                },
+                pyflakes = {
+                  enabled = false,
+                },
+                pylint = {
+                  enabled = true,
+                  executable = "venv/bin",
+                },
+              },
             },
           },
         },
-        efm = {
-          filetypes = { "python", "cpp", "lua" },
+        pyright = {},
+        -- rust_analyzer = {},
+        lua_ls = {
+          settings = {
+            Lua = {
+              workspace = {
+                checkThirdParty = false,
+              },
+              telemetry = { enable = false },
+              diagnostics = {
+                globals = { "vim" },
+              },
+            },
+          },
         },
+        -- efm = {
+        --   filetypes = { "python", "cpp", "lua" },
+        -- },
         html = {},
         cssls = {},
         tailwindcss = {},
@@ -84,19 +117,20 @@ return {
           },
         },
       },
+      -- you can do any additional lsp server setup here
+      -- return true if you don't want this server to be setup with lspconfig
+      ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
+      setup = {
+        -- example to setup with typescript.nvim
+        -- tsserver = function(_, opts)
+        --   require("typescript").setup({ server = opts })
+        --   return true
+        -- end,
+        -- Specify * to use this function as a fallback for any server
+        -- ["*"] = function(server, opts) end,
+      },
     },
-    -- you can do any additional lsp server setup here
-    -- return true if you don't want this server to be setup with lspconfig
-    ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
-    setup = {
-      -- example to setup with typescript.nvim
-      -- tsserver = function(_, opts)
-      --   require("typescript").setup({ server = opts })
-      --   return true
-      -- end,
-      -- Specify * to use this function as a fallback for any server
-      -- ["*"] = function(server, opts) end,
-    },
+    ---@param opts PluginLspOpts
     config = function(_, opts)
       -- LSP settings.
       --  This function gets run when an LSP connects to a particular buffer.
@@ -113,8 +147,8 @@ return {
           vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
         end
         -- Hover docs
-        nmap("<leader>k", ":Lspsaga hover_doc<CR>", "Hover Docs")
-        -- nmap("<leader>k", vim.lsp.buf.hover, "Hover Docs")
+        -- nmap("<leader>k", ":Lspsaga hover_doc<CR>", "Hover Docs")
+        nmap("<leader>k", vim.lsp.buf.hover, "Hover Docs")
         -- Code action
         nmap("<leader>ca", ":Lspsaga code_action<CR>", "[c]ode [a]ction")
         -- Go to definition
@@ -163,28 +197,75 @@ return {
 
       local servers = opts.servers
       -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
-      local capabilities = vim.lsp.protocol.make_client_capabilities()
-      capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
+      local capabilities = vim.tbl_deep_extend(
+        "force",
+        {},
+        vim.lsp.protocol.make_client_capabilities(),
+        require("cmp_nvim_lsp").default_capabilities(),
+        opts.capabilities or {}
+      )
 
-      -- enable mason
-      require("mason").setup()
+      local function setup(server)
+        local server_opts = vim.tbl_deep_extend("force", {
+          capabilities = vim.deepcopy(capabilities),
+          on_attach = on_attach,
+        }, servers[server] or {})
 
-      require("mason-lspconfig").setup({
-        -- list of servers for mason to install
-        ensure_installed = vim.tbl_keys(servers),
-        -- auto-install configured servers (with lspconfig)
-        automatic_installation = true, -- not the same as ensure_installed
-      })
+        if opts.setup[server] then
+          if opts.setup[server](server, server_opts) then
+            return
+          end
+        elseif opts.setup["*"] then
+          if opts.setup["*"](server, server_opts) then
+            return
+          end
+        end
+        require("lspconfig")[server].setup(server_opts)
+      end
 
-      require("mason-lspconfig").setup_handlers({
-        function(server_name)
-          require("lspconfig")[server_name].setup({
-            capabilities = capabilities,
-            on_attach = on_attach,
-            settings = servers[server_name],
-          })
-        end,
-      })
+      -- get all the servers that are available thourgh mason-lspconfig
+      local have_mason, mlsp = pcall(require, "mason-lspconfig")
+      local all_mslp_servers = {}
+      if have_mason then
+        all_mslp_servers = vim.tbl_keys(require("mason-lspconfig.mappings.server").lspconfig_to_package)
+      end
+
+      local ensure_installed = {} ---@type string[]
+      for server, server_opts in pairs(servers) do
+        if server_opts then
+          server_opts = server_opts == true and {} or server_opts
+          -- run manual setup if mason=false or if this is a server that cannot be installed with mason-lspconfig
+          if server_opts.mason == false or not vim.tbl_contains(all_mslp_servers, server) then
+            setup(server)
+          else
+            ensure_installed[#ensure_installed + 1] = server
+          end
+        end
+      end
+
+      if have_mason then
+        mlsp.setup({ ensure_installed = ensure_installed })
+        mlsp.setup_handlers({ setup })
+      end
+      -- -- enable mason
+      -- require("mason").setup()
+      --
+      -- require("mason-lspconfig").setup({
+      --   -- list of servers for mason to install
+      --   ensure_installed = vim.tbl_keys(servers),
+      --   -- auto-install configured servers (with lspconfig)
+      --   automatic_installation = true, -- not the same as ensure_installed
+      -- })
+      --
+      -- require("mason-lspconfig").setup_handlers({
+      --   function(server_name)
+      --     require("lspconfig")[server_name].setup({
+      --       capabilities = capabilities,
+      --       on_attach = on_attach,
+      --       { servers[server_name] },
+      --     })
+      --   end,
+      -- })
 
       -- configure typescript server with plugin
       require("typescript").setup({
@@ -193,6 +274,36 @@ return {
           on_attach = on_attach,
         },
       })
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    cmd = "Mason",
+    keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shfmt",
+        -- "flake8",
+      },
+    },
+    ---@param opts MasonSettings | {ensure_installed: string[]}
+    config = function(_, opts)
+      require("mason").setup(opts)
+      local mr = require("mason-registry")
+      local function ensure_installed()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local p = mr.get_package(tool)
+          if not p:is_installed() then
+            p:install()
+          end
+        end
+      end
+      if mr.refresh then
+        mr.refresh(ensure_installed)
+      else
+        ensure_installed()
+      end
     end,
   },
   {
