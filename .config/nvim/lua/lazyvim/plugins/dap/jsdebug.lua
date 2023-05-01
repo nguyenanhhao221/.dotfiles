e9ea61f8d1ca9bb62935546c1515d7918f04939c
Hao Nguyen <hao@haonguyen.tech>
2023-05-02 01:50:28 +0700

    feat(dap): adjust dap to work with dap-ui

--- .config/nvim/lua/lazyvim/plugins/dap/jsdebug.lua
+++ .config/nvim/lua/lazyvim/plugins/dap/jsdebug.lua
@@ -3,34 +3,8 @@ return {
   {
     "mfussenegger/nvim-dap",
     event = "VeryLazy",
-    dependencies = { "mxsdev/nvim-dap-vscode-js", "rcarriga/nvim-dap-ui" },
+    dependencies = { "mxsdev/nvim-dap-vscode-js" },
     config = function()
-      local dap_status, dap = pcall(require, "dap")
-      if not dap_status then
-        return
-      end
-
-      local nmap = function(keys, func, desc)
-        if desc then
-          desc = "Debug: " .. desc
-        end
-
-        vim.keymap.set("n", keys, func, { desc = desc })
-      end
-
-      -- Set keymaps to control the debugger
-      nmap("<F5>", dap.continue, "Debug continue")
-      nmap("<F10>", dap.step_over, "Debug step over")
-      nmap("<F11>", dap.step_into, "Debug step into")
-      nmap("<F12>", dap.step_out, "Debug step out")
-      nmap("<leader>b", dap.toggle_breakpoint, "Debug set Breakpoint")
-      nmap("<leader>B", function()
-        ---@diagnostic disable-next-line: param-type-mismatch
-        dap.set_breakpoint(vim.fn.input("Breakpoint condition: "))
-      end, "Breakpoint condition")
-
-      vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" })
-      vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
       local js_based_languages = { "typescript", "javascript", "typescriptreact" }
       for _, language in ipairs(js_based_languages) do
         require("dap").configurations[language] = {
@@ -110,71 +84,5 @@ return {
     "microsoft/vscode-js-debug",
     lazy = true,
     build = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-  },
-  {
-    "rcarriga/nvim-dap-ui",
-    event = "VeryLazy",
-    dependencies = { "mfussenegger/nvim-dap" },
-    opts = {
-
-      icons = { expanded = "▾", collapsed = "▸" },
-      mappings = {
-        expand = { "<CR>", "<2-LeftMouse>" },
-        open = "o",
-        remove = "d",
-        edit = "e",
-        repl = "r",
-        toggle = "t",
-      },
-      expand_lines = vim.fn.has("nvim-0.7"),
-      layouts = {
-        {
-          elements = {
-            -- Elements can be strings or table with id and size keys.
-            { id = "scopes", size = 0.25 },
-            "breakpoints",
-            "stacks",
-            "watches",
-          },
-          size = 40,
-          position = "left",
-        },
-        {
-          elements = {
-            { id = "repl", size = 0.5 },
-            { id = "console", size = 0.5 },
-          },
-          size = 10,
-          position = "bottom",
-        },
-      },
-      floating = {
-        max_height = nil, -- These can be integers or a float between 0 and 1.
-        max_width = nil, -- Floats will be treated as percentage of your screen.
-        border = "rounded", -- Border style. Can be "single", "double" or "rounded"
-        mappings = {
-          close = { "q", "<Esc>" },
-        },
-      },
-      windows = { indent = 1 },
-      render = {
-        max_type_length = nil,
-      },
-    },
-    config = function()
-      local dap, dapui = require("dap"), require("dapui")
-      dap.listeners.after.event_initialized["dapui_config"] = function()
-        vim.cmd("tabfirst|tabnext")
-        dapui.open()
-      end
-      dap.listeners.before.event_terminated["dapui_config"] = function()
-        dapui.close({})
-      end
-      dap.listeners.before.event_exited["dapui_config"] = function()
-        dapui.close({})
-      end
-
-      vim.keymap.set("n", "<leader>ui", require("dapui").toggle)
-    end,
   },
 }
