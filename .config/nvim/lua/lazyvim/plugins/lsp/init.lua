2d952455457d9f41cca82940e12d52d447df4801
Hao Nguyen <hao@haonguyen.tech>
2023-05-05 23:19:38 +0700

    chore: remove unused mason set up

--- .config/nvim/lua/lazyvim/plugins/lsp/init.lua
+++ .config/nvim/lua/lazyvim/plugins/lsp/init.lua
@@ -250,25 +250,6 @@ return {
         mlsp.setup({ ensure_installed = ensure_installed })
         mlsp.setup_handlers({ setup })
       end
-      -- -- enable mason
-      -- require("mason").setup()
-      --
-      -- require("mason-lspconfig").setup({
-      --   -- list of servers for mason to install
-      --   ensure_installed = vim.tbl_keys(servers),
-      --   -- auto-install configured servers (with lspconfig)
-      --   automatic_installation = true, -- not the same as ensure_installed
-      -- })
-      --
-      -- require("mason-lspconfig").setup_handlers({
-      --   function(server_name)
-      --     require("lspconfig")[server_name].setup({
-      --       capabilities = capabilities,
-      --       on_attach = on_attach,
-      --       { servers[server_name] },
-      --     })
-      --   end,
-      -- })
 
       -- configure typescript server with plugin
       require("typescript").setup({
