7895a2155590e92233688682290aeb2d5e676679
Hao Nguyen <hao@haonguyen.tech>
2023-05-05 23:18:18 +0700

    refactor: remove Format command cause not need anymore

--- .config/nvim/lua/lazyvim/plugins/lsp/init.lua
+++ .config/nvim/lua/lazyvim/plugins/lsp/init.lua
@@ -187,11 +187,6 @@ return {
           print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
         end, "[W]orkspace [L]ist Folders")
 
-        -- Create a command `:Format` local to the LSP buffer
-        vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
-          vim.lsp.buf.format()
-        end, { desc = "Format current buffer with LSP" })
-
         -- typescript specific keymaps (e.g. rename file and update imports)
         if client.name == "tsserver" then
           -- rename file and update imports
