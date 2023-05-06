74b9a1ad9c7975742491c7f32be4e4fd58b76d7f
Hao Nguyen <hao@haonguyen.tech>
2023-05-07 02:58:47 +0700

    refactor(nvim): separate opts for theme

--- .config/nvim/lua/lazyvim/plugins/colorschemes.lua
+++ .config/nvim/lua/lazyvim/plugins/colorschemes.lua
@@ -17,85 +17,87 @@ return {
     lazy = true, -- make sure we load this during startup if it is your main colorscheme
     -- priority = 1000, -- make sure to load this before all the other start plugins
     name = "kanagawa",
-    config = function()
-      -- load the colorscheme here
-      vim.cmd([[colorscheme kanagawa]])
-      require("kanagawa").setup({
-        compile = true, -- enable compiling the colorscheme
-        undercurl = true, -- enable undercurls
-        commentStyle = { italic = true },
-        functionStyle = {},
-        keywordStyle = { italic = true },
-        statementStyle = { bold = true },
-        typeStyle = {},
-        transparent = true, -- do not set background color
-        dimInactive = true, -- dim inactive window `:h hl-NormalNC`
-        terminalColors = true, -- define vim.g.terminal_color_{0,17}
-        theme = "wave", -- Load "wave" theme when 'background' option is not set
-        background = {
-          -- map the value of 'background' option to a theme
-          dark = "wave", -- try "dragon" !
-          light = "lotus",
-        },
-      })
-    end,
+    opts = {
+      compile = true, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = true, -- do not set background color
+      dimInactive = true, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      theme = "wave", -- Load "wave" theme when 'background' option is not set
+      background = {
+        -- map the value of 'background' option to a theme
+        dark = "wave", -- try "dragon" !
+        light = "lotus",
+      },
+    },
+    -- config = function(_, opts)
+    --   -- load the colorscheme here
+    --   vim.cmd([[colorscheme kanagawa]])
+    --   require("kanagawa").setup(opts)
+    -- end,
   },
   --Rose pine
   {
     "rose-pine/neovim",
     lazy = true,
     name = "rose-pine",
-    config = function()
-      require("rose-pine").setup({
-        --- @usage 'auto'|'main'|'moon'|'dawn'
-        variant = "auto",
-        --- @usage 'main'|'moon'|'dawn'
-        dark_variant = "main",
-        bold_vert_split = false,
-        dim_nc_background = false,
-        disable_background = false,
-        disable_float_background = false,
-        disable_italics = false,
+    opts = {
+      --- @usage 'auto'|'main'|'moon'|'dawn'
+      variant = "auto",
+      --- @usage 'main'|'moon'|'dawn'
+      dark_variant = "main",
+      bold_vert_split = false,
+      dim_nc_background = false,
+      disable_background = false,
+      disable_float_background = false,
+      disable_italics = false,
 
-        --- @usage string hex value or named color from rosepinetheme.com/palette
-        groups = {
-          background = "base",
-          background_nc = "_experimental_nc",
-          panel = "surface",
-          panel_nc = "base",
-          border = "highlight_med",
-          comment = "muted",
-          link = "iris",
-          punctuation = "subtle",
+      --- @usage string hex value or named color from rosepinetheme.com/palette
+      groups = {
+        background = "base",
+        background_nc = "_experimental_nc",
+        panel = "surface",
+        panel_nc = "base",
+        border = "highlight_med",
+        comment = "muted",
+        link = "iris",
+        punctuation = "subtle",
 
-          error = "love",
-          hint = "iris",
-          info = "foam",
-          warn = "gold",
+        error = "love",
+        hint = "iris",
+        info = "foam",
+        warn = "gold",
 
-          headings = {
-            h1 = "iris",
-            h2 = "foam",
-            h3 = "rose",
-            h4 = "gold",
-            h5 = "pine",
-            h6 = "foam",
-          },
-          -- or set all headings at once
-          -- headings = 'subtle'
+        headings = {
+          h1 = "iris",
+          h2 = "foam",
+          h3 = "rose",
+          h4 = "gold",
+          h5 = "pine",
+          h6 = "foam",
         },
+        -- or set all headings at once
+        -- headings = 'subtle'
+      },
 
-        -- Change specific vim highlight groups
-        -- https://github.com/rose-pine/neovim/wiki/Recipes
-        highlight_groups = {
-          ColorColumn = { bg = "rose" },
+      -- Change specific vim highlight groups
+      -- https://github.com/rose-pine/neovim/wiki/Recipes
+      highlight_groups = {
+        ColorColumn = { bg = "rose" },
 
-          -- Blend colours against the "base" background
-          CursorLine = { bg = "foam", blend = 10 },
-          StatusLine = { fg = "love", bg = "love", blend = 10 },
-        },
-      })
+        -- Blend colours against the "base" background
+        CursorLine = { bg = "foam", blend = 10 },
+        StatusLine = { fg = "love", bg = "love", blend = 10 },
+      },
+    },
+    config = function(_, opts)
       vim.cmd([[colorscheme rose-pine]])
+      require("rose-pine").setup(opts)
     end,
   },
   --Gruvbox
@@ -134,12 +136,11 @@ return {
   {
     "sainnhe/gruvbox-material",
     lazy = true,
-    name = "gruvbox-material",
-    config = function()
-      -- load the colorscheme here
-      vim.g.gruvbox_material.background = "hard"
-      vim.cmd([[colorscheme gruvbox-material]])
-    end,
+    opts = {
+      better_performance = 1,
+      background = "hard",
+    },
+    config = true,
   },
   -- catppuccin
   {
