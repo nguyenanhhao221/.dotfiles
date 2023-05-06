7bebf899e3322cebda8cdbef30fc88a1c7fddfd4
Hao Nguyen <hao@haonguyen.tech>
2023-05-06 19:54:54 +0700

    chore: add name for kanagawa

--- .config/nvim/lua/lazyvim/plugins/colorschemes.lua
+++ .config/nvim/lua/lazyvim/plugins/colorschemes.lua
@@ -17,7 +17,7 @@ return {
     "rebelot/kanagawa.nvim",
     lazy = true, -- make sure we load this during startup if it is your main colorscheme
     -- priority = 1000, -- make sure to load this before all the other start plugins
-    -- name = "kanagawa",
+    name = "kanagawa",
     config = function()
       -- load the colorscheme here
       vim.cmd([[colorscheme kanagawa]])
