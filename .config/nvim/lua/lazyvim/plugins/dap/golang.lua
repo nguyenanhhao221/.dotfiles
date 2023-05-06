6383efd3e45d908aedc9555667534f726233116a
Hao Nguyen <hao@haonguyen.tech>
2023-05-06 19:54:15 +0700

    feat(nvim-dap): add dap config for golang separate file

---
+++ .config/nvim/lua/lazyvim/plugins/dap/golang.lua
@@ -0,0 +1,22 @@
+return {
+  "leoluz/nvim-dap-go",
+  config = function()
+    local dap = require("dap")
+    dap.configurations.go = {
+      {
+        -- Must be "go" or it will be ignored by the plugin
+        type = "go",
+        name = "Attach remote",
+        mode = "remote",
+        request = "attach",
+      },
+      {
+        type = "go",
+        name = "Debug",
+        request = "launch",
+        program = "${file}",
+      },
+    }
+    require("dap-go").setup()
+  end,
+}
