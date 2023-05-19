a815827735634ddb601ea1476bd9bbef07545dfc
Hao Nguyen <hao@haonguyen.tech>
2023-05-20 01:37:52 +0700

    refactor(nvim): change gh plugin to VeryLazy

--- .config/nvim/lua/lazyvim/plugins/git.lua
+++ .config/nvim/lua/lazyvim/plugins/git.lua
@@ -65,7 +65,7 @@ return {
   {
     "ldelossa/gh.nvim",
     dependencies = { { "ldelossa/litee.nvim" } },
-    lazy = true,
+    event = "VeryLazy",
     config = function()
       require("litee.lib").setup()
       require("litee.gh").setup({})
