bf73a7ec3c5fefe3e3f32fba5161201cdfefd493
Hao Nguyen <hao@haonguyen.tech>
2023-05-28 18:26:24 +0700

    refactor: add back comment.nvim
    - mini.comment does not support block comment yet

--- .config/nvim/lua/lazyvim/plugins/init.lua
+++ .config/nvim/lua/lazyvim/plugins/init.lua
@@ -63,47 +63,9 @@ return {
   },
   --Use `gc` to comment
   {
-    "echasnovski/mini.comment",
-    version = "*",
+    "numToStr/Comment.nvim",
     event = "VeryLazy",
-    opts = {
-      -- No need to copy this inside `setup()`. Will be used automatically.
-      -- Options which control module behavior
-      options = {
-        -- Function to compute custom 'commentstring' (optional)
-        custom_commentstring = nil,
-
-        -- Whether to ignore blank lines
-        ignore_blank_line = false,
-
-        -- Whether to recognize as comment only lines without indent
-        start_of_line = false,
-
-        -- Whether to ensure single space pad for comment parts
-        pad_comment_parts = true,
-      },
-
-      -- Module mappings. Use `''` (empty string) to disable one.
-      mappings = {
-        -- Toggle comment (like `gcip` - comment inner paragraph) for both
-        -- Normal and Visual modes
-        comment = "gc",
-
-        -- Toggle comment on current line
-        comment_line = "gcc",
-
-        -- Define 'comment' textobject (like `dgc` - delete whole comment block)
-        textobject = "gc",
-      },
-
-      -- Hook functions to be executed at certain stage of commenting
-      hooks = {
-        -- Before successful commenting. Does nothing by default.
-        pre = function() end,
-        -- After successful commenting. Does nothing by default.
-        post = function() end,
-      },
-    },
+    config = true,
   },
   -- TODO comments
   {
@@ -162,6 +124,17 @@ return {
         -- or leave it empty to use the default settings
         -- refer to the configuration section below
       })
+    end,
+  },
+  -- {
+  --   dir = "/Users/haonguyen/Code/Lua/stackmap.nvim",
+  --   enabled = false,
+  -- },
+  {
+    "nvim-lua/plenary.nvim",
+    event = "VeryLazy",
+    config = function()
+      vim.api.nvim_set_keymap("n", "<leader>t", "<Plug>PlenaryTestFile", { noremap = false, silent = false })
     end,
   },
 }
