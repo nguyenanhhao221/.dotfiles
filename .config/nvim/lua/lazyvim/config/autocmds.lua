a76da21ae4bed55e3d6a15e72b129c5812a208dd
Hao Nguyen <hao@haonguyen.tech>
2023-06-22 23:21:15 +0700

    feat: add autocmds to get highlihght for  .env file

--- .config/nvim/lua/lazyvim/config/autocmds.lua
+++ .config/nvim/lua/lazyvim/config/autocmds.lua
@@ -15,5 +15,19 @@ vim.api.nvim_create_autocmd({ "BufRead" }, {
     vim.api.nvim_buf_set_option(0, "filetype", "jsonc")
   end,
 })
+
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+  pattern = { ".env", ".envrc" },
+  callback = function()
+    vim.filetype.add({
+      filename = {
+        [".env"] = "sh",
+        ["*.env"] = "sh",
+        [".envrc"] = "sh",
+        ["*.envrc"] = "sh",
+      },
+    })
+  end,
+})
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
