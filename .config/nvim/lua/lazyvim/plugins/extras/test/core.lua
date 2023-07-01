284839c5645692c2822cf8ddf70c6c198887a863
Hao Nguyen <hao@haonguyen.tech>
2023-07-01 23:05:09 +0700

    feat(dap): add run last test

--- .config/nvim/lua/lazyvim/plugins/extras/test/core.lua
+++ .config/nvim/lua/lazyvim/plugins/extras/test/core.lua
@@ -20,6 +20,7 @@ return {
       { "<leader>tT", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
       { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
       { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
+      { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last Test" },
       { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
       { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
       { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop" },
