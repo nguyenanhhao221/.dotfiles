4329a7bd55d5ce4871965ce869720bdee2569391
Hao Nguyen <hao@haonguyen.tech>
2023-07-04 23:00:08 +0700

    feat: add custom dap python for benerator

--- .config/nvim/lua/lazyvim/plugins/dap/python.lua
+++ .config/nvim/lua/lazyvim/plugins/dap/python.lua
@@ -8,6 +8,17 @@ return {
     },
     config = function()
       local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
+      local dap = require("dap")
+      dap.configurations.python = {
+        {
+          -- Must be "go" or it will be ignored by the plugin
+          name = "Python: Module",
+          type = "python",
+          module = "benerator",
+          request = "launch",
+          justMyCode = true,
+        },
+      }
       require("dap-python").setup(mason_path .. "packages/debugpy/venv/bin/python")
     end,
   },
