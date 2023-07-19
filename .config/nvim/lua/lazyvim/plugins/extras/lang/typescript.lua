9b049fb479167b07937f9952324c4b9e9fb3cbdf
Hao Nguyen <hao@haonguyen.tech>
2023-07-19 17:17:57 +0700

    feat(nvim): add neotest-vitest adapter

--- .config/nvim/lua/lazyvim/plugins/extras/lang/typescript.lua
+++ .config/nvim/lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -60,4 +60,13 @@ return {
       },
     },
   },
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "marilari88/neotest-vitest",
+    },
+    opts = function(_, opts)
+      table.insert(opts.adapters, require("neotest-vitest")({}))
+    end,
+  },
 }
