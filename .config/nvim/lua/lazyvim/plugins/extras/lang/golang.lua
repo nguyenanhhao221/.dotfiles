d3eeebf2fc8f7c32de3e7097554799c68e3e1ce6
Hao Nguyen <hao@haonguyen.tech>
2023-07-24 22:34:38 +0700

    feat: add key map for neotest go lang debug

--- .config/nvim/lua/lazyvim/plugins/extras/lang/golang.lua
+++ .config/nvim/lua/lazyvim/plugins/extras/lang/golang.lua
@@ -8,5 +8,9 @@ return {
     opts = function(_, opts)
       table.insert(opts.adapters, require("neotest-go")({}))
     end,
+    keys = {
+      -- Workaround for the lack of a DAP strategy in neotest-go: https://github.com/nvim-neotest/neotest-go/issues/12
+      { "<leader>td", "<cmd>lua require('dap-go').debug_test()<CR>", desc = "Debug Nearest (Go)" },
+    },
   },
 }
