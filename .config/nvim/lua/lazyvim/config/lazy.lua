82946467ecad10893937b7e5bd59f0e587f1e213
Hao Nguyen <hao@haonguyen.tech>
2023-09-07 12:00:00 +0700

    chore: disbale netrw

--- .config/nvim/lua/lazyvim/config/lazy.lua
+++ .config/nvim/lua/lazyvim/config/lazy.lua
@@ -47,7 +47,7 @@ require("lazy").setup({
         "gzip",
         -- "matchit",
         -- "matchparen",
-        "netrwPlugin",
+        -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
