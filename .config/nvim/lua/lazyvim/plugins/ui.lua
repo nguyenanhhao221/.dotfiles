1c933f6f3eca692e5a07a4485ad199acabbfb949
Hao Nguyen <hao@haonguyen.tech>
2023-10-14 00:21:32 +0700

    feat(nvim): adjust option for indent-blankline to version 3

--- .config/nvim/lua/lazyvim/plugins/ui.lua
+++ .config/nvim/lua/lazyvim/plugins/ui.lua
@@ -112,12 +112,29 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = { "BufReadPost", "BufNewFile" },
     opts = {
-      -- char = "▏",
-      char = "│",
-      filetype_exclude = { "help", "alpha", "dashboard", "neo-tree", "nvim-tree", "Trouble", "lazy" },
-      show_trailing_blankline_indent = false,
-      show_current_context = false,
+      indent = {
+        char = "│",
+        tab_char = "│",
+        show_trailing_blankline_indent = false,
+        show_current_context = false,
+      },
+      scope = { enabled = false },
+      exclude = {
+        filetypes = {
+          "help",
+          "alpha",
+          "dashboard",
+          "neo-tree",
+          "Trouble",
+          "lazy",
+          "mason",
+          "notify",
+          "toggleterm",
+          "lazyterm",
+        },
+      },
     },
+    main = "ibl",
   },
   -- dashboard
   {
