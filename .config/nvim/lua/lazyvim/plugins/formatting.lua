9b72cefd6b0e70c5134965e3716bbd013e6e006d
Hao Nguyen <hao@haonguyen.tech>
2023-11-07 14:53:50 +0700

    chore: place holder for ruff_format in Conform nvim

--- .config/nvim/lua/lazyvim/plugins/formatting.lua
+++ .config/nvim/lua/lazyvim/plugins/formatting.lua
@@ -20,7 +20,11 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         -- Conform will run multiple formatters sequentially
-        python = { "isort", "black" },
+        python = {
+          "isort",
+          "black",
+          -- "ruff_format",
+        },
         -- Use a sub-list to run only the first available formatter
         javascript = { { "prettierd", "prettier" } },
       },
