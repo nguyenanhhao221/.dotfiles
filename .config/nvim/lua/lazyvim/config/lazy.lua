bb92f27a8910a51aadb63eea2703670543f61e92
Hao Nguyen <hao@haonguyen.tech>
2023-11-24 17:41:15 +0700

    make catppuccin theme

--- .config/nvim/lua/lazyvim/config/lazy.lua
+++ .config/nvim/lua/lazyvim/config/lazy.lua
@@ -42,7 +42,7 @@ require("lazy").setup({
       version = false, -- always use the latest git commit
       -- version = "*", -- try installing the latest stable version for plugins that support semver
     },
-    install = { colorscheme = { "kanagawa" } },
+    install = { colorscheme = { "catppuccin" } },
     checker = { enabled = false }, -- automatically check for plugin updates
     performance = {
       rtp = {
