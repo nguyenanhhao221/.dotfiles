5b2433d075a9be87a6ee6d2a1670eb1c923b4205
Hao Nguyen <hao@haonguyen.tech>
2023-11-27 23:42:16 +0700

    refactor(oil): disable some keymap in oil

--- .config/nvim/lua/lazyvim/plugins/editor/init.lua
+++ .config/nvim/lua/lazyvim/plugins/editor/init.lua
@@ -98,15 +98,21 @@ return {
       -- Oil will take over directory buffers (e.g. `vim .` or `:e src/`)
       -- Set to false if you still want to use netrw.
       default_file_explorer = true,
+      -- Keymaps in oil buffer. Can be any value that `vim.keymap.set` accepts OR a table of keymap
+      -- options with a `callback` (e.g. { callback = function() ... end, desc = "", mode = "n" })
+      -- Additionally, if it is a string that matches "actions.<name>",
+      -- it will use the mapping at require("oil.actions").<name>
+      -- Set to `false` to remove a keymap
+      -- See :help oil-actions for a list of all available actions
       keymaps = {
         ["g?"] = "actions.show_help",
         ["<CR>"] = "actions.select",
-        ["<C-s>"] = "actions.select_vsplit",
-        ["<C-h>"] = "actions.select_split",
+        ["<C-s>"] = false,
+        ["<C-h>"] = false,
         ["<C-t>"] = "actions.select_tab",
         ["<C-p>"] = "actions.preview",
         ["<C-c>"] = "actions.close",
-        ["<C-l>"] = "actions.refresh",
+        ["<C-l>"] = false,
         ["-"] = "actions.parent",
         ["_"] = "actions.open_cwd",
         ["`"] = "actions.cd",
@@ -115,6 +121,8 @@ return {
       },
       -- Set to false to disable all of the above keymaps
       use_default_keymaps = true,
+      -- Skip the confirmation popup for simple operations
+      skip_confirm_for_simple_edits = true,
       view_options = {
         -- Show files and directories that start with "."
         show_hidden = true,
