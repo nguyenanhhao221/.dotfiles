87cdd0bbf6c742bfd6e1a688c179a138dbb7b251
Hao Nguyen <hao@haonguyen.tech>
2023-12-09 00:43:08 +0700

    feat: vue use prettier as formater

--- .config/nvim/lua/lazyvim/plugins/formatting.lua
+++ .config/nvim/lua/lazyvim/plugins/formatting.lua
@@ -29,6 +29,7 @@ return {
         -- Use a sub-list to run only the first available formatter
         javascript = { { "prettierd", "prettier" } },
         typescript = { { "prettierd", "prettier" } },
+        vue = { { "prettierd", "prettier" } },
         typescriptreact = { { "prettierd", "prettier" } },
       },
       format_on_save = function(bufnr)
