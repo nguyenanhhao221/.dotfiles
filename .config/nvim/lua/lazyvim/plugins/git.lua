a7ce8529d3c54318e45e1120a3e62c496b50a74c
Hao Nguyen <hao@haonguyen.tech>
2023-12-18 22:39:10 +0700

    feat(gitlab): add keymaps for gitlab.nvim

--- .config/nvim/lua/lazyvim/plugins/git.lua
+++ .config/nvim/lua/lazyvim/plugins/git.lua
@@ -134,7 +134,7 @@ return {
   },
   {
     "harrisoncramer/gitlab.nvim",
-    event = "VeryLazy",
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
@@ -146,8 +146,32 @@ return {
     build = function()
       require("gitlab.server").build(true)
     end, -- Builds the Go binary
-    config = function()
-      require("gitlab").setup()
+    dir = "/Users/haonguyen/Code/neovim-projects/gitlab-nvim", -- Pass in the path to your cloned repository
+    opts = {
+      -- Point to my local .gitlab.nvim path which is ignore by git to avoid expose token
+      -- As of today need to have auth for rapiddweller gitlab
+      config_path = "/Users/haonguyen/.dotfiles/.config/", -- Custom path for `.gitlab.nvim` file, please read the "Connecting to Gitlab" section
+    },
+    -- stylua: ignore
+    config = function(_, opts)
+      local gitlab = require("gitlab")
+      gitlab.setup(opts)
+      vim.keymap.set("n", "glr", gitlab.review, { silent = false, desc = "gitlab review" })
+      vim.keymap.set("n", "gls", gitlab.summary, { silent = false, desc = "gitlab summary" })
+      vim.keymap.set("n", "glA", gitlab.approve, { silent = false, desc = "gitlab approve" })
+      vim.keymap.set("n", "glR", gitlab.revoke, { silent = false, desc = "gitlab revoke" })
+      vim.keymap.set("n", "glc", gitlab.create_comment, { silent = false, desc = "gitlab create comment" })
+      vim.keymap.set("v", "glc", gitlab.create_multiline_comment, { silent = false, desc = "gitlab create multiline comment" })
+      vim.keymap.set("v", "glC", gitlab.create_comment_suggestion, { silent = false, desc = "gitlab comment suggestion" })
+      vim.keymap.set("n", "glm", gitlab.move_to_discussion_tree_from_diagnostic, { silent = false, desc = "gitlab discussion tree" })
+      vim.keymap.set("n", "gln", gitlab.create_note, { silent = false, desc = "gitlab create note" })
+      vim.keymap.set("n", "gld", gitlab.toggle_discussions, { silent = false, desc = "gitlab toggle discussions" })
+      vim.keymap.set("n", "glaa", gitlab.add_assignee, { silent = false, desc = "gitlab add assignee" })
+      vim.keymap.set("n", "glad", gitlab.delete_assignee, { silent = false, desc = "gitlab detele assignee" })
+      vim.keymap.set("n", "glra", gitlab.add_reviewer, { silent = false, desc = "gitlab add reviewer" })
+      vim.keymap.set("n", "glrd", gitlab.delete_reviewer, { silent = false, desc = "gitlab delete reviewer" })
+      vim.keymap.set("n", "glp", gitlab.pipeline, { silent = false, desc = "gitlab pipeline" })
+      vim.keymap.set("n", "glo", gitlab.open_in_browser, { silent = false, desc = "gitlab open in browser" })
     end,
   },
   {
