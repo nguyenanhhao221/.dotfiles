e8fe79c5ab09d7a447d7c910476c1c650db91cb8
Hao Nguyen <hao@haonguyen.tech>
2023-12-23 23:58:31 +0700

    feat: use typescript tools.nvim
    - typescript.nvim is depreciated

--- .config/nvim/lua/lazyvim/plugins/extras/lang/typescript.lua
+++ .config/nvim/lua/lazyvim/plugins/extras/lang/typescript.lua
@@ -10,53 +10,65 @@ return {
   },
 
   -- correctly setup lspconfig
+  -- {
+  --   "neovim/nvim-lspconfig",
+  --   dependencies = { "jose-elias-alvarez/typescript.nvim" },
+  --   opts = {
+  --     -- make sure mason installs the server
+  --     servers = {
+  --       ---@type lspconfig.options.tsserver
+  --       tsserver = {
+  --         keys = {
+  --           { "<leader>oi", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
+  --           { "<leader>rf", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
+  --           { "<leader>ru", "<cmd>TypescriptRemoveUnused<CR>", desc = "[R]emove unused [T]ypescript" },
+  --         },
+  --         settings = {
+  --           javascript = {
+  --             inlayHints = {
+  --               includeInlayEnumMemberValueHints = true,
+  --               includeInlayFunctionLikeReturnTypeHints = true,
+  --               includeInlayFunctionParameterTypeHints = true,
+  --               includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+  --               includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+  --               includeInlayPropertyDeclarationTypeHints = true,
+  --               includeInlayVariableTypeHints = true,
+  --             },
+  --           },
+  --           typescript = {
+  --             inlayHints = {
+  --               includeInlayEnumMemberValueHints = true,
+  --               includeInlayFunctionLikeReturnTypeHints = true,
+  --               includeInlayFunctionParameterTypeHints = true,
+  --               includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
+  --               includeInlayParameterNameHintsWhenArgumentMatchesName = true,
+  --               includeInlayPropertyDeclarationTypeHints = true,
+  --               includeInlayVariableTypeHints = true,
+  --             },
+  --           },
+  --           completions = {
+  --             completeFunctionCalls = true,
+  --           },
+  --         },
+  --       },
+  --     },
+  --     setup = {
+  --       tsserver = function(_, opts)
+  --         require("typescript").setup({ server = opts })
+  --         return true
+  --       end,
+  --     },
+  --   },
+  -- },
   {
-    "neovim/nvim-lspconfig",
-    dependencies = { "jose-elias-alvarez/typescript.nvim" },
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    ft = { "javascript", "typescript", "typescriptreact", "javascriptreact" },
     opts = {
-      -- make sure mason installs the server
-      servers = {
-        ---@type lspconfig.options.tsserver
-        tsserver = {
-          keys = {
-            { "<leader>oi", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
-            { "<leader>rf", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
-            { "<leader>ru", "<cmd>TypescriptRemoveUnused<CR>", desc = "[R]emove unused [T]ypescript" },
-          },
-          settings = {
-            javascript = {
-              inlayHints = {
-                includeInlayEnumMemberValueHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayVariableTypeHints = true,
-              },
-            },
-            typescript = {
-              inlayHints = {
-                includeInlayEnumMemberValueHints = true,
-                includeInlayFunctionLikeReturnTypeHints = true,
-                includeInlayFunctionParameterTypeHints = true,
-                includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all';
-                includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-                includeInlayPropertyDeclarationTypeHints = true,
-                includeInlayVariableTypeHints = true,
-              },
-            },
-            completions = {
-              completeFunctionCalls = true,
-            },
-          },
+      settings = {
+        tsserver_file_preferences = {
+          includeInlayParameterNameHints = "all",
         },
-      },
-      setup = {
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
-        end,
       },
     },
   },
