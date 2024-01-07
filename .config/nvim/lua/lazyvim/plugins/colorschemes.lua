475ff693be3d29a32fedfca414f825152e82f036
Hao Nguyen <hao@haonguyen.tech>
2024-01-08 02:54:45 +0700

    feat: move to gruvbox

--- .config/nvim/lua/lazyvim/plugins/colorschemes.lua
+++ .config/nvim/lua/lazyvim/plugins/colorschemes.lua
@@ -14,8 +14,8 @@ return {
   --Kanagawa Themes
   {
     "rebelot/kanagawa.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
+    lazy = true, -- make sure we load this during startup if it is your main colorscheme
+    -- priority = 1000, -- make sure to load this before all the other start plugins
     name = "kanagawa",
     opts = {
       compile = false, -- enable compiling the colorscheme
@@ -97,10 +97,32 @@ return {
   --Gruvbox
   {
     "ellisonleao/gruvbox.nvim",
-    lazy = true, -- make sure we load this during startup if it is your main colorscheme
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000,
     name = "gruvbox",
     opts = {
-      contrast = "soft", -- can be "hard", "soft" or empty string
+      terminal_colors = true, -- add neovim terminal colors
+      undercurl = true,
+      underline = true,
+      bold = true,
+      italic = {
+        strings = false,
+        emphasis = false,
+        comments = true,
+        operators = false,
+        folds = true,
+      },
+      strikethrough = true,
+      invert_selection = false,
+      invert_signs = false,
+      invert_tabline = false,
+      invert_intend_guides = false,
+      inverse = true, -- invert background for search, diffs, statuslines and errors
+      contrast = "", -- can be "hard", "soft" or empty string
+      palette_overrides = {},
+      overrides = {},
+      dim_inactive = false,
+      transparent_mode = true,
     },
     config = function(_, opts)
       require("gruvbox").setup(opts)
