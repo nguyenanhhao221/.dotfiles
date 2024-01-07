5720180df585b0593644291fa36919c701942985
Hao Nguyen <hao@haonguyen.tech>
2024-01-08 02:58:38 +0700

    feat: add leetcode nvim

---
+++ .config/nvim/lua/lazyvim/plugins/leetcode.lua
@@ -0,0 +1,29 @@
+local leet_arg = "leetcode.nvim"
+return {
+  "kawre/leetcode.nvim",
+  lazy = leet_arg ~= vim.fn.argv()[1],
+  build = ":TSUpdate html",
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+    "nvim-lua/plenary.nvim", -- required by telescope
+    "MunifTanjim/nui.nvim",
+
+    -- optional
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
+  },
+  opts = {
+    -- configuration goes here
+    lang = "go",
+
+    keys = {
+      toggle = { "q", "<Esc>" }, ---@type string|string[]
+      confirm = { "<CR>" }, ---@type string|string[]
+
+      reset_testcases = "r", ---@type string
+      use_testcase = "U", ---@type string
+      focus_testcases = "H", ---@type string
+      focus_result = "L", ---@type string
+    },
+  },
+}
