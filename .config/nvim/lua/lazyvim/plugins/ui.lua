fbdc7ccc5a852a7761c53e7e461b36bbd86fe972
Hao Nguyen <hao@haonguyen.tech>
2024-01-08 02:56:45 +0700

    feat: tabline for dashboard

--- .config/nvim/lua/lazyvim/plugins/ui.lua
+++ .config/nvim/lua/lazyvim/plugins/ui.lua
@@ -51,6 +51,7 @@ return {
           -- this is taken care of by lualine
           -- enabling this messes up the actual laststatus setting after loading a file
           statusline = false,
+          tabline = true,
         },
         config = {
           header = vim.split(logo, "\n"),
