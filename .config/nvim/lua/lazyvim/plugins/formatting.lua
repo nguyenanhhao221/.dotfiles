8d7a3bf3cf2f91b1c4ec42f662ca1e8229355efe
Hao Nguyen <hao@haonguyen.tech>
2024-01-17 00:55:54 +0700

    refactor(conform): notify on error

--- .config/nvim/lua/lazyvim/plugins/formatting.lua
+++ .config/nvim/lua/lazyvim/plugins/formatting.lua
@@ -17,7 +17,7 @@ return {
       },
     },
     opts = {
-      notify_on_error = false,
+      notify_on_error = true,
       formatters_by_ft = {
         lua = { "stylua" },
         -- Conform will run multiple formatters sequentially
