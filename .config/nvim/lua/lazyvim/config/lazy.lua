4cb224d2ef4412d3f683b90de563a0ccdbe69204
Hao Nguyen <hao@haonguyen.tech>
2024-03-23 18:49:03 +0700

    refactor: move lazy config

--- .config/nvim/lua/lazyvim/config/lazy.lua
+++ .config/nvim/lua/lazyvim/config/lazy.lua
@@ -35,31 +35,37 @@ require("lazy").setup({
     { import = "lazyvim.plugins.dap.jsdebug" },
     { import = "lazyvim.plugins" },
   },
-  {
-    defaults = {
-      -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-      -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-      lazy = false,
-      -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
-      -- have outdated releases, which may break your Neovim install.
-      version = false, -- always use the latest git commit
-      -- version = "*", -- try installing the latest stable version for plugins that support semver
-    },
-    install = { colorscheme = { "catppuccin" } },
-    checker = { enabled = false }, -- automatically check for plugin updates
-    performance = {
-      rtp = {
-        -- disable some rtp plugins
-        disabled_plugins = {
-          "gzip",
-          -- "matchit",
-          -- "matchparen",
-          -- "netrwPlugin",
-          "tarPlugin",
-          "tohtml",
-          "tutor",
-          "zipPlugin",
-        },
+}, {
+  defaults = {
+    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
+    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
+    lazy = false,
+    -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
+    -- have outdated releases, which may break your Neovim install.
+    version = false, -- always use the latest git commit
+    -- version = "*", -- try installing the latest stable version for plugins that support semver
+  },
+  dev = {
+    -- directory where you store your local plugin projects
+    path = "~/Code",
+    ---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
+    patterns = {}, -- For example {"folke"}
+    fallback = false, -- Fallback to git when local plugin doesn't exist
+  },
+  install = { colorscheme = { "catppuccin" } },
+  checker = { enabled = false }, -- automatically check for plugin updates
+  performance = {
+    rtp = {
+      -- disable some rtp plugins
+      disabled_plugins = {
+        "gzip",
+        -- "matchit",
+        -- "matchparen",
+        "netrwPlugin",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
       },
     },
   },
