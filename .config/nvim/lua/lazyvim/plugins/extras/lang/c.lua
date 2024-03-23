d3b651c8f1255378f23587bf9b0a6e5d45e854b8
Hao Nguyen <hao@haonguyen.tech>
2024-03-23 18:49:54 +0700

    refactor(c-config): try to add dap, disable lint

--- .config/nvim/lua/lazyvim/plugins/extras/lang/c.lua
+++ .config/nvim/lua/lazyvim/plugins/extras/lang/c.lua
@@ -7,29 +7,72 @@ return {
       end
     end,
   },
-  {
-    "williamboman/mason.nvim",
-    opts = function(_, opts)
-      opts.ensure_installed = opts.ensure_installed or {}
-      vim.list_extend(opts.ensure_installed, { "clangd", "clang-format", "cpplint" })
-    end,
-  },
-
-  {
-    "mfussenegger/nvim-lint",
-    optional = true,
-    opts = {
-      linters_by_ft = {
-        c = { "cpplint" },
-      },
-    },
-  },
+  -- {
+  --   "mfussenegger/nvim-lint",
+  --   optional = true,
+  --   opts = {
+  --     linters_by_ft = {
+  --       c = { "cpplint" },
+  --     },
+  --   },
+  -- },
   {
     "neovim/nvim-lspconfig",
     opts = {
       servers = {
         clangd = {},
       },
+    },
+  },
+
+  --DAP for C, C++, Cpp
+  {
+    "mfussenegger/nvim-dap",
+    optional = true,
+    -- config = function()
+    --   local mason_path = vim.fn.glob(vim.fn.stdpath("data") .. "/mason/")
+    --   local dap = require("dap")
+    --   dap.adapters.cppdbg = {
+    --     id = "cppdbg",
+    --     type = "executable",
+    --     command = mason_path .. "packages/cpptools/extension/debugAdapters/bin/OpenDebugAD7",
+    --   }
+    --   dap.configurations.c = {
+    --     {
+    --       name = "Launch file",
+    --       type = "cppdbg",
+    --       request = "launch",
+    --       program = function()
+    --         return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    --       end,
+    --       cwd = "${workspaceFolder}",
+    --       stopAtEntry = true,
+    --     },
+    --     {
+    --       name = "Attach to gdbserver :1234",
+    --       type = "cppdbg",
+    --       request = "launch",
+    --       MIMode = "gdb",
+    --       miDebuggerServerAddress = "localhost:1234",
+    --       miDebuggerPath = "/usr/bin/gdb",
+    --       cwd = "${workspaceFolder}",
+    --       program = function()
+    --         return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+    --       end,
+    --     },
+    --   }
+    --   require("dap.ext.vscode").load_launchjs(nil)
+    -- end,
+  },
+  {
+    "jay-babu/mason-nvim-dap.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "mfussenegger/nvim-dap",
+    },
+    opts = {
+      handlers = {},
     },
   },
 }
