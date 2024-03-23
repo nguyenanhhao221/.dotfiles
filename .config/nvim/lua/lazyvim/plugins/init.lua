baaf20aef1ec1f1e433c7663b2f3de2986b69df3
Hao Nguyen <hao@haonguyen.tech>
2024-03-23 18:52:05 +0700

    refactor(nvim-tmux): add key shortcut

--- .config/nvim/lua/lazyvim/plugins/init.lua
+++ .config/nvim/lua/lazyvim/plugins/init.lua
@@ -96,7 +96,23 @@ return {
   -- Detect tabstop and shiftwidth automatically
   { "tpope/vim-sleuth", event = "VeryLazy" },
   --Navigate between window easier, work with tmux and nvim
-  { "christoomey/vim-tmux-navigator", event = "VeryLazy" },
+  {
+    "christoomey/vim-tmux-navigator",
+    cmd = {
+      "TmuxNavigateLeft",
+      "TmuxNavigateDown",
+      "TmuxNavigateUp",
+      "TmuxNavigateRight",
+      "TmuxNavigatePrevious",
+    },
+    keys = {
+      { "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+      { "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+      { "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+      { "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+      { "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+    },
+  },
 
   --Which key when press
   {
