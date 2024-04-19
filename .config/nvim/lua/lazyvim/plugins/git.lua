367c2a479444a071cfca0050a0b52f321fa093e6
Hao Nguyen <hao@haonguyen.tech>
2024-04-20 01:38:34 +0700

    refactor(git): attach gitsigns to un track file

--- .config/nvim/lua/lazyvim/plugins/git.lua
+++ .config/nvim/lua/lazyvim/plugins/git.lua
@@ -187,6 +187,7 @@ return {
         changedelete = { text = "▎" },
         untracked = { text = "▎" },
       },
+      attach_to_untracked = true,
       current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
         virt_text = true,
