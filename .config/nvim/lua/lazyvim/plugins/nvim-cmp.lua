758265296f236f7b808afb24a0595fc48427ac90
Hao Nguyen <hao@haonguyen.tech>
2024-04-20 01:57:57 +0700

    refactor(cmp): better lua snip

--- .config/nvim/lua/lazyvim/plugins/nvim-cmp.lua
+++ .config/nvim/lua/lazyvim/plugins/nvim-cmp.lua
@@ -6,9 +6,30 @@ return {
     "hrsh7th/cmp-nvim-lsp",
     "hrsh7th/cmp-buffer",
     "hrsh7th/cmp-path",
-    "L3MON4D3/LuaSnip",
+    {
+      "L3MON4D3/LuaSnip",
+      build = (function()
+        -- Build Step is needed for regex support in snippets.
+        -- This step is not supported in many windows environments.
+        -- Remove the below condition to re-enable on windows.
+        if vim.fn.has("win32") == 1 or vim.fn.executable("make") == 0 then
+          return
+        end
+        return "make install_jsregexp"
+      end)(),
+      dependencies = {
+        -- `friendly-snippets` contains a variety of premade snippets.
+        --    See the README about individual language/framework/plugin snippets:
+        --    https://github.com/rafamadriz/friendly-snippets
+        {
+          "rafamadriz/friendly-snippets",
+          config = function()
+            require("luasnip.loaders.from_vscode").lazy_load()
+          end,
+        },
+      },
+    },
     "saadparwaiz1/cmp_luasnip",
-    "rafamadriz/friendly-snippets",
     "onsails/lspkind.nvim",
     "hrsh7th/cmp-cmdline",
     "hrsh7th/cmp-nvim-lsp-signature-help",
@@ -18,9 +39,6 @@ return {
     local cmp = require("cmp")
     local luasnip = require("luasnip")
     local lspkind = require("lspkind")
-
-    --load friendly-snippets
-    require("luasnip/loaders/from_vscode").lazy_load()
 
     vim.completeopt = "menu,menuone,noselect"
 
