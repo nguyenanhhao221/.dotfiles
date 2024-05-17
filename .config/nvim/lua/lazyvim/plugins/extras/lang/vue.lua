525ea930c59f8ea259f16934a7bf12057aba9679
Hao Nguyen <hao@haonguyen.tech>
2024-05-17 21:34:23 +0700

    fix(lsp): vue config

--- .config/nvim/lua/lazyvim/plugins/extras/lang/vue.lua
+++ .config/nvim/lua/lazyvim/plugins/extras/lang/vue.lua
@@ -2,36 +2,38 @@
 -- enable take over mode, disable tsserver
 -- TODO: should improve for monorepo
 return {
-  "nvim-lspconfig",
-  opts = function(_, opts)
-    local cwd = vim.fn.getcwd()
-    local util = require("lspconfig.util")
-    local project_root = util.find_node_modules_ancestor(cwd)
+  {
+    "nvim-lspconfig",
+    config = function(_, opts)
+      local cwd = vim.fn.getcwd()
+      local util = require("lspconfig.util")
+      local project_root = util.find_node_modules_ancestor(cwd)
 
-    local vue_path = util.path.join(project_root, "node_modules", "vue")
-    local is_vue = vim.fn.isdirectory(vue_path) == 1
-    if is_vue then
-      opts.servers.volar = {
-        filetypes = {
-          "vue",
-          "javascript",
-          "javascript.jsx",
-          "typescript",
-          "typescript.tsx",
-          "javascriptreact",
-          "typescriptreact",
-          "json",
-        },
-      }
-      opts.servers.tsserver = {
-        autostart = false,
-        root_dir = function()
-          return false
-        end,
-        single_file_support = false,
-      }
-    end
+      local vue_path = util.path.join(project_root, "node_modules", "vue")
+      local is_vue = vim.fn.isdirectory(vue_path) == 1
+      if is_vue then
+        opts.servers.volar = {
+          filetypes = {
+            "vue",
+            "javascript",
+            "javascript.jsx",
+            "typescript",
+            "typescript.tsx",
+            "javascriptreact",
+            "typescriptreact",
+            "json",
+          },
+        }
+        opts.servers.tsserver = {
+          autostart = false,
+          root_dir = function()
+            return false
+          end,
+          single_file_support = false,
+        }
+      end
 
-    return opts
-  end,
+      return opts
+    end,
+  },
 }
