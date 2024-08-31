017d13fe793867e539d12d91f119462b79bbcb42
Hao Nguyen <hao@haonguyen.tech>
2024-09-01 05:28:43 +0700

    chore: remove types

--- .config/nvim/lua/plugins/colorschemes.lua
+++ .config/nvim/lua/plugins/colorschemes.lua
@@ -11,8 +11,6 @@ return {
       style = "night", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
       transparent = false, -- Enable this to disable setting the background color
       terminal_colors = true, -- Configure the colors used when opening a `:terminal` in [Neovim](https://github.com/neovim/neovim)
-      ---@param highlights Highlights
-      ---@param colors ColorScheme
       on_highlights = function(highlights, colors)
         highlights.TelescopeSelection = {
           bg = colors.bg_highlight,
