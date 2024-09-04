8e69870ca118c240df8aa51bec3df1aea4da0809
Hao Nguyen <hao@haonguyen.tech>
2024-09-05 04:03:27 +0700

    chore: neotest keymap

--- .config/nvim/lua/plugins/extras/test/core.lua
+++ .config/nvim/lua/plugins/extras/test/core.lua
@@ -9,7 +9,7 @@ return {
 	-- stylua: ignore
     keys = {
       { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
-      { "<leader>tT", function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run All Test Files" },
+      { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
       { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
       { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
       { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last Test" },
@@ -84,7 +84,8 @@ return {
     optional = true,
     -- stylua: ignore
     keys = {
-      { "<leader>td", function() require("neotest").run.run({strategy = "dap"}) end, desc = "Debug Nearest" },
+      { "<leader>td", function() require("neotest").run.run({suite = false,strategy = "dap"}) end, desc = "Debug Nearest" },
+			{ "<leader>tD", function() require("neotest").run.run({ suite = true, strategy = "dap" }) end, desc = "Debug Test Run All Suite" },
     },
   },
 }
