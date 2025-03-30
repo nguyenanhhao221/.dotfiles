f05ac9c2b2f94b7d5795bdd5b0620d38e50bd2cb
Hao Nguyen <hao@haonguyen.tech>
2025-03-30 16:21:39 +0700

    chore: move nvim-test coverage to core test plugin

--- .config/nvim/lua/plugins/extras/test/core.lua
+++ .config/nvim/lua/plugins/extras/test/core.lua
@@ -92,4 +92,33 @@ return {
 			{ "<leader>tD", function() require("neotest").run.run({ suite = true, strategy = "dap" }) end, desc = "Debug Test Run All Suite" },
     },
   },
+  --
+  -- For test coverage
+  {
+    "andythigpen/nvim-coverage",
+    enabled = true,
+    event = "VeryLazy",
+    config = function()
+      require("coverage").setup({
+        commands = true, -- create commands
+        highlights = {
+          -- customize highlight groups created by the plugin
+          covered = { fg = "#C3E88D" }, -- supports style, fg, bg, sp (see :h highlight-gui)
+          uncovered = { fg = "#F07178" },
+        },
+        signs = {
+          -- use your own highlight groups or text markers
+          covered = { hl = "CoverageCovered", text = "▎" },
+          uncovered = { hl = "CoverageUncovered", text = "▎" },
+        },
+        summary = {
+          -- customize the summary pop-up
+          min_coverage = 80.0, -- minimum coverage threshold (used for highlighting)
+        },
+        lang = {
+          -- customize language specific settings
+        },
+      })
+    end,
+  },
 }
