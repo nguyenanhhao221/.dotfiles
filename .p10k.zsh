105dc1677ead9a414a5a463a3fc733bb1e50189c
Hao Nguyen <hao@haonguyen.tech>
2025-06-12 00:49:53 +0700

    feat(p10k): add NodeJS custom icon

--- .p10k.zsh
+++ .p10k.zsh
@@ -1051,7 +1051,7 @@
   # Show node version only when in a directory tree containing package.json.
   typeset -g POWERLEVEL9K_NODE_VERSION_PROJECT_ONLY=true
   # Custom icon.
-  # typeset -g POWERLEVEL9K_NODE_VERSION_VISUAL_IDENTIFIER_EXPANSION='⭐'
+  typeset -g POWERLEVEL9K_NODE_VERSION_VISUAL_IDENTIFIER_EXPANSION='NodeJS'
 
   #######################[ go_version: go version (https://golang.org) ]########################
   # Go version color.
