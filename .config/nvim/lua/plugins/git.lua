6e19071d4a6427cec9f6d019865f13fb415f3a36
Hao Nguyen <hao@haonguyen.tech>
2025-06-14 03:15:45 +0700

    refactor(git): not attach to untrack and disable line blame

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -184,8 +184,8 @@ return {
         topdelete = { text = "‾" },
         changedelete = { text = "~" },
       },
-      attach_to_untracked = true,
-      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      attach_to_untracked = false,
+      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
       current_line_blame_opts = {
         virt_text = true,
         virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
