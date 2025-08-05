22cad41c56dda3c6220d63aec64e0a987ce5a70d
Hao Nguyen <hao@haonguyen.tech>
2025-08-06 01:48:20 +0700

    refactor: improve neotest core options handling

--- .config/nvim/lua/plugins/neo_test.lua
+++ .config/nvim/lua/plugins/neo_test.lua
@@ -43,6 +43,7 @@ return {
     },
     config = function(_, opts)
       local neotest_ns = vim.api.nvim_create_namespace("neotest")
+      local neotest = require("neotest")
       vim.diagnostic.config({
         virtual_text = {
           format = function(diagnostic)
@@ -52,34 +53,28 @@ return {
           end,
         },
       }, neotest_ns)
+      opts = vim.deepcopy(opts)
 
-      if opts.adapters then
-        local adapters = {}
-        for name, config in pairs(opts.adapters or {}) do
-          if type(name) == "number" then
-            if type(config) == "string" then
-              config = require(config)
-            end
-            adapters[#adapters + 1] = config
-          elseif config ~= false then
-            local adapter = require(name)
-            if type(config) == "table" and not vim.tbl_isempty(config) then
-              local meta = getmetatable(adapter)
-              if adapter.setup then
-                adapter.setup(config)
-              elseif meta and meta.__call then
-                adapter(config)
-              else
-                error("Adapter " .. name .. " does not support setup")
-              end
-            end
-            adapters[#adapters + 1] = adapter
+      local adapters = {}
+      for k, v in pairs(opts.adapters) do
+        if type(v) == "function" then
+          table.insert(adapters, require(k)(v()))
+        elseif v then
+          table.insert(adapters, require(k)(v))
+        else
+          table.insert(adapters, require(k))
+        end
+      end
+      opts.adapters = adapters
+      if opts.consumers then
+        for k, v in pairs(opts.consumers) do
+          if type(v) == "function" then
+            opts.consumers[k] = v()
           end
         end
-        opts.adapters = adapters
       end
 
-      require("neotest").setup(opts)
+      neotest.setup(opts)
     end,
   },
   {
