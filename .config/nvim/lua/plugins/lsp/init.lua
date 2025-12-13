baf79bf0938a8ccb75c9c35a9a231d9a526a02c2
Hao Nguyen <hao@haonguyen.tech>
2025-12-13 19:38:06 +0700

    feat: add codesettings.nvim

--- .config/nvim/lua/plugins/lsp/init.lua
+++ .config/nvim/lua/plugins/lsp/init.lua
@@ -7,9 +7,38 @@ return {
       -- Automatically install LSPs and related tools to stdpath for Neovim
       { "mason-org/mason.nvim", config = true, cmd = "Mason" }, -- NOTE: Must be loaded before dependents
       {
-        "folke/neoconf.nvim",
-        cmd = "Neoconf",
-        opts = {},
+        "mrjones2014/codesettings.nvim",
+        -- these are the default settings just set `opts = {}` to use defaults
+        opts = {
+          ---Look for these config files
+          config_file_paths = { ".vscode/settings.json", "codesettings.json", "lspsettings.json" },
+          ---Integrate with jsonls to provide LSP completion for LSP settings based on schemas
+          jsonls_integration = true,
+          ---Set up library paths for lua_ls automatically to pick up the generated type
+          ---annotations provided by codesettings.nvim; to enable for only your nvim config,
+          ---you can also do something like:
+          ---lua_ls_integration = function()
+          ---  return vim.uv.cwd() == ('%s/.config/nvim'):format(vim.env.HOME)
+          ---end,
+          ---This integration also works for emmylua_ls
+          lua_ls_integration = true,
+          ---Set filetype to jsonc when opening a file specified by `config_file_paths`,
+          ---make sure you have the jsonc tree-sitter parser installed for highlighting
+          jsonc_filetype = true,
+          ---Enable live reloading of settings when config files change; for servers that support it,
+          ---this is done via the `workspace/didChangeConfiguration` notification, otherwise the
+          ---server is restarted
+          live_reload = false,
+          ---Provide your own root dir; can be a string or function returning a string.
+          ---It should be/return the full absolute path to the root directory.
+          ---If not set, defaults to `require('codesettings.util').get_root()`
+          root_dir = nil,
+          --- How to merge lists; 'append' (default), 'prepend' or 'replace'
+          merge_lists = "append",
+        },
+        -- I recommend loading on these filetype so that the
+        -- jsonls integration, lua_ls integration, and jsonc filetype setup works
+        ft = { "json", "jsonc", "lua" },
       },
     },
     opts = function()
@@ -134,6 +163,13 @@ return {
         "sqlfluff", -- Format sql
       })
 
+      -- set up for codesettings.nvim
+      vim.lsp.config("*", {
+        before_init = function(_, config)
+          local codesettings = require("codesettings")
+          codesettings.with_local_settings(config.name, config)
+        end,
+      })
       -- Loop through all servers and set them up with their configurations
       for server_name, server_settings in pairs(servers) do
         -- Merge default capabilities with any server-specific settings
