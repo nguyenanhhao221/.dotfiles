935d1cb2ff0fd73e9e2569137efa213804fdc07a
Hao Nguyen <hao@haonguyen.tech>
2025-12-13 19:03:00 +0700

    chore: disable markdownlint

--- .config/nvim/lua/plugins/extras/lang/markdown.lua
+++ .config/nvim/lua/plugins/extras/lang/markdown.lua
@@ -38,7 +38,7 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        markdown = { "markdownlint" },
+        -- markdown = { "markdownlint" },
       },
     },
   },
