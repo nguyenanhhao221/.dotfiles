ba1190cf559334c93035aeb404ebd1a3fbf270d6
Hao Nguyen <hao@haonguyen.tech>
2025-12-15 21:45:59 +0700

    feat: add render-markdown.nvim to view markdown better while in nvim

--- .config/nvim/lua/plugins/extras/lang/markdown.lua
+++ .config/nvim/lua/plugins/extras/lang/markdown.lua
@@ -62,6 +62,14 @@ return {
       vim.cmd([[do FileType]])
     end,
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    ft = "markdown",
+    dependencies = { "nvim-treesitter/nvim-treesitter", "nvim-tree/nvim-web-devicons" }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  },
 
   -- {
   --   "lukas-reineke/headlines.nvim",
