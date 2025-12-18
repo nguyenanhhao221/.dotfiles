4b72518b285d747eb3bec1d7983372b4124ac284
Hao Nguyen <hao@haonguyen.tech>
2025-12-18 23:38:21 +0700

    refactor: clean up neotest keymap

--- .config/nvim/lua/plugins/neo_test.lua
+++ .config/nvim/lua/plugins/neo_test.lua
@@ -7,16 +7,19 @@ return {
       "nvim-treesitter/nvim-treesitter",
     },
 	-- stylua: ignore
-    keys = {
-      { "<leader>tt", function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run File" },
-      { "<leader>tT", function() require("neotest").run.run(vim.uv.cwd()) end, desc = "Run All Test Files" },
-      { "<leader>tr", function() require("neotest").run.run() end, desc = "Run Nearest" },
-      { "<leader>ts", function() require("neotest").summary.toggle() end, desc = "Toggle Summary" },
-      { "<leader>tl", function() require("neotest").run.run_last() end, desc = "Run Last Test" },
-      { "<leader>to", function() require("neotest").output.open({ enter = true, auto_close = true }) end, desc = "Show Output" },
-      { "<leader>tO", function() require("neotest").output_panel.toggle() end, desc = "Toggle Output Panel" },
-      { "<leader>tS", function() require("neotest").run.stop() end, desc = "Stop" },
-    },
+    keys = function()
+			nt = require("neotest")
+      return {
+        { "<leader>tt", function() nt.run.run(vim.fn.expand("%")) end, desc = "Run File", },
+        { "<leader>tT", function() nt.run.run(vim.uv.cwd()) end, desc = "Run All Test Files",},
+        { "<leader>tr", function() nt.run.run() end, desc = "Run Nearest", }, { "<leader>ts", function() nt.summary.toggle() end, desc = "Toggle Summary",},
+        { "<leader>tl", function() nt.run.run_last() end, desc = "Run Last Test", },
+				{ "<leader>to", function() nt.output.open({ enter = true, auto_close = true }) end, desc = "Show Output", },
+        { "<leader>tO", function() nt.output_panel.toggle() end, desc = "Toggle Output Panel", },
+				{ "<leader>tS", function() nt.run.stop() end, desc = "Stop", },
+				{ "<leader>ta", function() nt.run.attach() end, desc = "Attach to a running process for the given position", },
+      }
+    end,
     opts = {
       discovery = {
         concurrent = 4, -- Adjust to performance issue with test discovery with pytest pytest
