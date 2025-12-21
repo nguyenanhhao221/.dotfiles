baf36f076ecc37980dbaa89d5f086d55e7ae5deb
Hao Nguyen <hao@haonguyen.tech>
2025-12-21 20:20:21 +0700

    feat: migrate treesitter to main branch from master

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,44 +1,22 @@
 return {
   --Treesitter
   {
-    -- Highlight, edit, and navigate code "nvim-treesitter/nvim-treesitter",
     "nvim-treesitter/nvim-treesitter",
-    -- https://github.com/nvim-treesitter/nvim-treesitter/commit/42fc28ba918343ebfd5565147a42a26580579482
-    -- Use master branch for now, in the future Treesitter may switch to main branch as the default
-    branch = "master",
-    init = function(plugin)
-      -- PERF: add nvim-treesitter queries to the rtp and it's custom query predicates early
-      -- This is needed because a bunch of plugins no longer `require("nvim-treesitter")`, which
-      -- no longer trigger the **nvim-treesitter** module to be loaded in time.
-      -- Luckily, the only things that those plugins need are the custom queries, which we make available
-      -- during startup.
-      require("lazy.core.loader").add_to_rtp(plugin)
-      require("nvim-treesitter.query_predicates")
-    end,
-    event = { "VeryLazy" },
-    lazy = vim.fn.argc(-1) == 0, -- load treesitter early when opening a file from the cmdline
-    cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
+    -- lazy = false,
+    event = "VeryLazy",
     build = ":TSUpdate",
-    -- Additional text objects via treesitter
-    keys = {
-      { "<leader>tp", "<cmd>InspectTree<CR>", desc = "Treesitter Playground Toggle" },
-    },
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      --Treesitter context for sticky scroll
-      {
-        "nvim-treesitter/nvim-treesitter-context",
-        dependencies = "nvim-treesitter",
-        config = true,
-      },
-    },
-    ---@type TSConfig
-    opts = {
-      -- A list of parser names, or "all" (the four listed parsers should always be installed)
-      ensure_installed = {
+    config = function()
+      local ts = require("nvim-treesitter")
+      local parsers = {
         "c",
+        "dockerfile",
         "cpp",
         "go",
+        "gitconfig",
+        "gitcommit",
+        "gitrebase",
+        "gitignore",
+        "diff",
         "lua",
         "python",
         "toml",
@@ -56,61 +34,216 @@ return {
         "gitignore",
         "javascript",
         "json",
+        "jsonc",
         "css",
         "html",
-      },
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
+        "yaml",
+        "make",
+        "tmux",
+      }
 
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
+      for _, parser in ipairs(parsers) do
+        ts.install(parser)
+      end
 
-      textobjects = {
+      -- Enable fold
+      vim.wo[0][0].foldexpr = "v:lua.vim.treesitter.foldexpr()"
+      vim.wo[0][0].foldmethod = "expr"
+      vim.api.nvim_command("set nofoldenable")
+
+      -- Enable indentation only
+      if vim.bo.filetype ~= "python" then
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end
+
+      -- Run treesitter base on the FileType
+      vim.api.nvim_create_autocmd("FileType", {
+        -- pattern = "*",
+        callback = function()
+          -- vim.treesitter.start()
+          pcall(vim.treesitter.start)
+        end,
+      })
+    end,
+  },
+  -- tree-sitter-context.lua
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    event = "VeryLazy",
+    branch = "main",
+    dependencies = { "nvim-treesitter/nvim-treesitter", branch = "main" },
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+    end,
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
         select = {
-          enable = true,
-          lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-          keymaps = {
-            -- You can use the capture groups defined in textobjects.scm
-            ["aa"] = "@parameter.outer",
-            ["ia"] = "@parameter.inner",
-            ["af"] = "@function.outer",
-            ["if"] = "@function.inner",
-            ["ac"] = "@class.outer",
-            ["ic"] = "@class.inner",
+          -- Automatically jump forward to textobj, similar to targets.vim
+          lookahead = true,
+          -- You can choose the select mode (default is charwise 'v')
+
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "V", -- linewise
+            ["@class.outer"] = "<c-v>", -- blockwise
           },
+          include_surrounding_whitespace = false,
         },
-      },
-
-      highlight = {
-        -- `false` will disable the whole extension
-        enable = true,
-
-        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-        -- Using this option may slow down your editor, and you may see some duplicate highlights.
-        -- Instead of true it can also be a list of languages
-        additional_vim_regex_highlighting = false,
-      },
-      indent = { enable = true, disable = { "python" } },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "gnn",
-          node_incremental = "grn",
-          scope_incremental = "grc",
-          node_decremental = "grm",
+        move = {
+          -- whether to set jumps in the jumplist
+          set_jumps = true,
         },
-      },
-    },
-    ---@param opts TSConfig
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-      -- require("vim.treesitter.query").set(
-      --   "markdown",
-      --   "highlights",
-      --   "(fenced_code_block_delimiter) @punctuation.delimiter"
-      -- )
+      })
+
+      -- Selects
+      local select = require("nvim-treesitter-textobjects.select")
+      vim.keymap.set({ "x", "o" }, "af", function()
+        select.select_textobject("@function.outer", "textobjects")
+      end, { desc = "Select outer function" })
+      vim.keymap.set({ "x", "o" }, "if", function()
+        select.select_textobject("@function.inner", "textobjects")
+      end, { desc = "Select inner function" })
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        select.select_textobject("@class.outer", "textobjects")
+      end, { desc = "Select outer class" })
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        select.select_textobject("@class.inner", "textobjects")
+      end, { desc = "Select inner class" })
+      -- You can also use captures from other query groups like `locals.scm`
+      vim.keymap.set({ "x", "o" }, "as", function()
+        select.select_textobject("@local.scope", "locals")
+      end, { desc = "Select scope" })
+
+      -- Swaps
+      local swap = require("nvim-treesitter-textobjects.swap")
+      vim.keymap.set("n", "<leader>a", function()
+        swap.swap_next("@parameter.inner")
+      end, { desc = "Swap next parameter" })
+      vim.keymap.set("n", "<leader>A", function()
+        swap.swap_previous("@parameter.outer")
+      end, { desc = "Swap previous parameter" })
+
+      local move = require("nvim-treesitter-textobjects.move")
+      vim.keymap.set({ "n", "x", "o" }, "]m", function()
+        move.goto_next_start("@function.outer", "textobjects")
+      end, { desc = "Next function start" })
+      vim.keymap.set({ "n", "x", "o" }, "]]", function()
+        move.goto_next_start("@class.outer", "textobjects")
+      end, { desc = "Next class start" })
+      -- You can also pass a list to group multiple queries.
+      vim.keymap.set({ "n", "x", "o" }, "]o", function()
+        move.goto_next_start({ "@loop.inner", "@loop.outer" }, "textobjects")
+      end, { desc = "Next loop start" })
+      -- You can also use captures from other query groups like `locals.scm` or `folds.scm`
+      vim.keymap.set({ "n", "x", "o" }, "]s", function()
+        move.goto_next_start("@local.scope", "locals")
+      end, { desc = "Next scope start" })
+      vim.keymap.set({ "n", "x", "o" }, "]z", function()
+        move.goto_next_start("@fold", "folds")
+      end, { desc = "Next fold start" })
+
+      vim.keymap.set({ "n", "x", "o" }, "]f", function()
+        move.goto_next_end("@function.outer", "textobjects")
+      end, { desc = "Next function end" })
+      vim.keymap.set({ "n", "x", "o" }, "][", function()
+        move.goto_next_end("@class.outer", "textobjects")
+      end, { desc = "Next class end" })
+
+      vim.keymap.set({ "n", "x", "o" }, "[m", function()
+        move.goto_previous_start("@function.outer", "textobjects")
+      end, { desc = "Previous function start" })
+      vim.keymap.set({ "n", "x", "o" }, "[[", function()
+        move.goto_previous_start("@class.outer", "textobjects")
+      end, { desc = "Previous class start" })
+
+      vim.keymap.set({ "n", "x", "o" }, "[M", function()
+        move.goto_previous_end("@function.outer", "textobjects")
+      end, { desc = "Previous function end" })
+      vim.keymap.set({ "n", "x", "o" }, "[]", function()
+        move.goto_previous_end("@class.outer", "textobjects")
+      end, { desc = "Previous class end" })
+
+      -- Go to either the start or the end, whichever is closer.
+      -- Use if you want more granular movements
+      vim.keymap.set({ "n", "x", "o" }, "]d", function()
+        move.goto_next("@conditional.outer", "textobjects")
+      end, { desc = "Next conditional" })
+      vim.keymap.set({ "n", "x", "o" }, "[d", function()
+        move.goto_previous("@conditional.outer", "textobjects")
+      end, { desc = "Previous conditional" })
+
+      local ts_repeat_move = require("nvim-treesitter-textobjects.repeatable_move")
+
+      -- Repeat movement with ; and ,
+      -- ensure ; goes forward and , goes backward regardless of the last direction
+      vim.keymap.set(
+        { "n", "x", "o" },
+        ";",
+        ts_repeat_move.repeat_last_move_next,
+        { desc = "Repeat last move forward" }
+      )
+      vim.keymap.set(
+        { "n", "x", "o" },
+        ",",
+        ts_repeat_move.repeat_last_move_previous,
+        { desc = "Repeat last move backward" }
+      )
+
+      -- Optionally, make builtin f, F, t, T also repeatable with ; and ,
+      vim.keymap.set(
+        { "n", "x", "o" },
+        "f",
+        ts_repeat_move.builtin_f_expr,
+        { expr = true, desc = "Find char forward (repeatable)" }
+      )
+      vim.keymap.set(
+        { "n", "x", "o" },
+        "F",
+        ts_repeat_move.builtin_F_expr,
+        { expr = true, desc = "Find char backward (repeatable)" }
+      )
+      vim.keymap.set(
+        { "n", "x", "o" },
+        "t",
+        ts_repeat_move.builtin_t_expr,
+        { expr = true, desc = "Till char forward (repeatable)" }
+      )
+      vim.keymap.set(
+        { "n", "x", "o" },
+        "T",
+        ts_repeat_move.builtin_T_expr,
+        { expr = true, desc = "Till char backward (repeatable)" }
+      )
+    end,
+  },
+  --Treesitter context for sticky scroll
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = { "nvim-treesitter", branch = "main" },
+    event = "VeryLazy",
+    config = function()
+      tc = require("treesitter-context")
+      tc.setup({
+        enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+        multiwindow = false, -- Enable multiwindow support.
+        max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+        min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+        line_numbers = true,
+        multiline_threshold = 20, -- Maximum number of lines to show for a single context
+        trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+        mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+        -- Separator between context and content. Should be a single character string, like '-'.
+        -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+        separator = nil,
+        zindex = 20, -- The Z-index of the context window
+        on_attach = nil, -- (fun(buf: integer): boolean) return false to disable attaching
+      })
+
+      vim.keymap.set("n", "[c", function()
+        tc.go_to_context(vim.v.count1)
+      end, { silent = false, desc = "Jumping to context (upwards)" })
     end,
   },
 }
